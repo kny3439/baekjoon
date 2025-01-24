e0d6f1ecf0f3a8ce87d74296652f4656de5cc095
kny3439 <kny8664@naver.com>
2025-01-24 16:55:13 +0900

    [Bronze I] Title: 단어 공부, Time: 492 ms, Memory: 45128 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,54 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String str = sc.next();
+		str=str.toUpperCase();
+		
+		
+		HashMap<Character, Integer> map = new HashMap<>();
+		
+		for(int i=0; i<str.length(); i++) {
+			
+			char c=str.charAt(i);
+			
+			if(map.containsKey(c)) {
+				
+				map.put(c, map.get(c)+1);
+				
+			}else {
+				map.put(c, 1);
+			}
+		
+		}
+		
+		int max=0;
+		char result='?';
+		
+		for(Map.Entry<Character, Integer>entry: map.entrySet()) {
+			
+			if(entry.getValue()>max) {
+				max=entry.getValue();
+				result=entry.getKey();
+			}else if(entry.getValue()==max) {
+				result='?';
+			}
+		}
+		
+	
+		
+		
+		System.out.println(result);
+		
+
+		
+		
+	}
+
+}
